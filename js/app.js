f666ef5b8eaaa8bc8a9404b898bc0d80b6c97576
kieronk <kieron.kirkland@gmail.com>
2014-11-19 18:54:57 +0000

    added sound and throwing

--- js/app.js
+++ js/app.js
@@ -8,11 +8,16 @@ $(document).ready(function() {
 		$(".ryu-still").show();
 	})
 	.mousedown(function() {
-		// play hadouken 
+		playHadouken();
 		$(".ryu-ready").hide();
 		$(".ryu-throwing").show();
-		$(".hadouken").show();
-		// show hadouken and move it to right side of screen
+		$(".hadouken").finish().show().animate(
+			{"left": "300px"},
+			500, 
+			function() {
+				$(this).hide();
+				$(this).css("left", "-212px");
+			});
 	})
 	.mouseup(function() {
 		$(".ryu-throwing").hide();
@@ -21,3 +26,9 @@ $(document).ready(function() {
 	});
 
 })
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
